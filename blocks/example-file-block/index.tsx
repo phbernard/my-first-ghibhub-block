f9a050ffe0ebda6bb7c5dd8b38c89ee78365e49b
Philippe Bernard <philippe.bernard@gmail.com>
2023-02-28 19:32:24 +0100

    Nstallers passed as MDX

--- blocks/example-file-block/index.tsx
+++ blocks/example-file-block/index.tsx
@@ -3,6 +3,19 @@ import { Nstaller, Connector } from "@nstaldev/react-core";
 import { Box } from "@primer/react";
 import AllComponents from "../nstal-components";
 import { CreateFile } from '@nstaldev/react-core';
+import MDX from "@mdx-js/runtime";
+
+
+const nstaller = `
+# Hello nstal!
+
+This is a sample nstaller.
+
+<CreateFile path="another-file.txt" content="Some interesting content" />
+
+Et voilà !
+`;
+
 
 export default function (props: FileBlockProps) {
   const { context, content, metadata, onUpdateMetadata } = props;
@@ -10,15 +23,18 @@ export default function (props: FileBlockProps) {
     ? getLanguageFromFilename(context.path)
     : "N/A";
 
+  const components = {
+    CreateFile
+  };
+
   return (
     <Box p={4}>
       <Nstaller components={AllComponents}>
-        Hey!
         <Connector />
 
-        Now do it:
-
-        <CreateFile path="the-file.txt" content="Hey!!" />
+        <MDX components={components} scope={{ cf: 'Hihi' }}>
+          {nstaller}
+        </MDX>
       </Nstaller>
     </Box>
   );
