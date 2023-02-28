f77f49a2737a29a3bfbaf69082208199c20e2ff9
Philippe Bernard <philippe.bernard@gmail.com>
2023-02-28 19:33:16 +0100

    Styled Wrapper with animations

--- blocks/nstal-components/Wrapper.stories.tsx
+++ blocks/nstal-components/Wrapper.stories.tsx
@@ -3,12 +3,28 @@ import React from 'react';
 import { Meta, StoryObj } from "@storybook/react";
 
 import Wrapper from './Wrapper';
-import { ActionWrapperProps } from '@nstaldev/react-core'
+import { ActionStatus, ActionWrapperProps } from '@nstaldev/react-core'
+
+const statusOptions = {
+  'Later': ActionStatus.Later,
+  'NextToRun': ActionStatus.NextToRun,
+  'Starting': ActionStatus.Starting,
+  'Running': ActionStatus.Running,
+  'Completed': ActionStatus.Completed,
+  'Error': ActionStatus.Error
+};
 
 export default {
   title: 'Wrapper',
   component: Wrapper,
   argTypes: {
+    status: {
+      options: statusOptions,
+      control: {
+        type: 'select',
+        labels: statusOptions
+      }
+    }
   },
 } as Meta;
 
@@ -16,7 +32,12 @@ type Story = StoryObj<typeof Wrapper>;
 
 export const Default: Story = {
   args: {
-    children: <div>TODO</div>,
+    children: <div>
+      <h1>Hey!</h1>
+      <p>
+        This is some random content.
+      </p>
+    </div>,
     status: 'hello',
     automated: false
   }
--- blocks/nstal-components/Wrapper.tsx
+++ blocks/nstal-components/Wrapper.tsx
@@ -1,14 +1,44 @@
-import { ActionWrapperProps } from '@nstaldev/react-core'
+import { ActionWrapperProps, ActionStatus } from '@nstaldev/react-core'
 import { Box } from "@primer/react";
 
-const Wrapper = (props: ActionWrapperProps) => (
-  <Box sx={{
-    transitionProperty: 'padding,backgroundColor',
-    p: props.automated ? 2 : 0,
-    backgroundColor: props.automated ? 'accent.subtle' : 'background.default'
-  }}>
-    {props.children}
-  </Box>
-)
+const Wrapper = (props: ActionWrapperProps) => {
+  let colorFamily;
+  switch(props.status) {
+    case(ActionStatus.Later):
+    colorFamily = 'neutral';
+    break;
+
+    case(ActionStatus.NextToRun):
+    case(ActionStatus.Running):
+    case(ActionStatus.Starting):
+    colorFamily = 'accent';
+    break;
+
+    case(ActionStatus.Completed):
+    colorFamily = 'success';
+    break;
+
+    case(ActionStatus.Error):
+    colorFamily = 'danger';
+    break;
+  }
+
+  return (
+    <Box sx={{
+      transitionProperty: 'padding background-color boxShadow border-color',
+      transitionDuration: '250ms',
+      py: 2,
+      px: props.automated ? 2 : 0,
+      borderRadius: 1,
+      backgroundColor: !props.automated ? 'background.default' : `${colorFamily}.subtle`,
+      borderColor: props.automated ? `${colorFamily}.muted` : 'transparent',
+      borderWidth: 1,
+      borderStyle: 'solid',
+      boxShadow: props.automated ? 'shadow.medium' : undefined
+    }}>
+      {props.children}
+    </Box>
+  )
+};
 
 export default Wrapper;
