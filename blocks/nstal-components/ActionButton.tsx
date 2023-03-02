93fc0e6186b49856e7682d3ac05fabce67b919c4
Philippe Bernard <philippe.bernard@gmail.com>
2023-03-02 12:10:54 +0100

    Spinner when the action is running

--- blocks/nstal-components/ActionButton.tsx
+++ blocks/nstal-components/ActionButton.tsx
@@ -1,10 +1,17 @@
-import { ActionButtonProps } from '@nstaldev/react-core'
-import { Button } from '@primer/react'
+import { ActionButtonProps, ActionStatus } from '@nstaldev/react-core'
+import { Button, Spinner } from '@primer/react'
 
-const ActionButton = (props: ActionButtonProps) => (
-  <Button onClick={props.onClick}>
-    Run
-  </Button>
-)
+const ActionButton = (props: ActionButtonProps) => {
+  const inProgress = props.status === ActionStatus.Starting || props.status === ActionStatus.Running;
+
+  return (
+    <Button
+      onClick={props.onClick}
+      disabled={inProgress}
+    >
+      {inProgress ? <Spinner size='small' /> : <>Run</>}
+    </Button>
+  )
+}
 
 export default ActionButton;
